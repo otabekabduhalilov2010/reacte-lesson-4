bd86cc67d8a220d11d99cc04d88f89f2d5f78510
otabekabduhalilov2010 <otabekabduhalilov80@gmail.com>
2024-10-31 21:48:01 +0500

    Add files via upload

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Hero.scss'
+
+const Hero = () => {
+  return (
+
+    <>
+    <section className="hero">
+        <div className="container">
+             <h1 className='hero__title'>A brand and product designer working with clients globally</h1>
+             <div className="hero_box">
+
+                <p>Expertise</p>
+                <p>Branding</p>
+                <p>Product</p>
+                <p>Design Systems</p>
+
+
+             </div>
+        </div>
+    </section>
+    </>
+  )
+}
+
+export default Hero
